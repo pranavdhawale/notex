3731b5fd4f58d704728b67e13f110fb67e476ca5
Pranav Dhawale <pranavdhawale19@gmail.com>
2026-02-06 10:33:35 +0530

    feat(server): implement dynamic TTL for rooms
    Added ExpireAt field to Room model, configured MongoDB TTL index, and updated API handlers to manage expiration logic based on content presence.

--- server/internal/state/mongo.go
+++ server/internal/state/mongo.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -28,5 +29,19 @@ func InitMongo(uri string, dbName string) {
 
 	MongoClient = client
 	MongoDatabase = client.Database(dbName)
+	
+	// Create TTL Index for Dynamic Expiration
+	roomsCollection := MongoDatabase.Collection("rooms")
+	indexModel := mongo.IndexModel{
+		Keys: bson.M{"expire_at": 1},
+		Options: options.Index().SetExpireAfterSeconds(0), // Expire exactly at the time specified in expire_at
+	}
+	_, err = roomsCollection.Indexes().CreateOne(ctx, indexModel)
+	if err != nil {
+		log.Printf("Failed to create TTL index: %v", err)
+	} else {
+		log.Println("TTL Index created on rooms.expire_at")
+	}
+
 	log.Println("Connected to MongoDB")
 }
